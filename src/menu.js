e5fd3a2556ca810cca2ae4c748acb5780c9506d9
LeoRoma <xiajtn@gmail.com>
2019-12-12 18:02:52 +0000

    #printMenu function added

--- src/menu.js
+++ src/menu.js
@@ -1,11 +1,24 @@
-class Menu{
+class Menu {
   constructor() {
     this.list = [];
   };
 
-  addDish(dish){
-    this.list.push({name: dish.name, price: dish.price});
+  addDish(dish) {
+    this.list.push({ name: dish.name, price: dish.price });
+    // console.log(this.list);
   }
+
+  printMenu() {
+    let menu = this.list.map((item, index) => {
+      return `${index + 1}. ${item.name}: £${item.price}`;
+   });
+    // for(let i = 0; i < this.list.length; i++){
+      
+    //   `${i +1}. ${this.list[1].name}: £${this.list[i].price}`;
+
+    // };
+   return menu.join();
+  };
 };
 
 module.exports = Menu;
--- test/menu.test.js
+++ test/menu.test.js
@@ -6,10 +6,17 @@ describe('Menu', () => {
   beforeEach(() => {
     menu = new Menu;
     pizza = new Dish('pizza', 1);
+    panino = new Dish('panino', 2);
   });
 
   test('adding dish into a list', () => {
     menu.addDish(pizza);
     expect(menu.list).toEqual([{name: 'pizza', price: 1}]);
   })
+
+  test('printing the menu', () => {
+    menu.addDish(pizza);
+    menu.addDish(panino);
+    expect(menu.printMenu()).toEqual('1. pizza: £1,2. panino: £2');
+  });
 });
